c0786eda019089cf20d0afa02ff00f559f88eb18
kdoh52 <kdoh52@gmail.com>
2020-07-25 20:58:09 -0700

    quiz flow & calc points

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 var start = document.getElementById("start");
 var secLeft = document.getElementById("secLeft");
 var introText = document.getElementById("intro");
+var showPoints = document.getElementById("userScore");
+
 var question1 = document.getElementById("q1");
 var question2 = document.getElementById("q2");
 var question3 = document.getElementById("q3");
@@ -8,10 +10,36 @@ var question4 = document.getElementById("q4");
 var question5 = document.getElementById("q5");
 var finish = document.getElementById("finish");
 
+var answer1_1 = document.querySelector("#a1-1");
+var answer1_2 = document.querySelector("#a1-2");
+var answer1_3 = document.querySelector("#a1-3");
+var answer1_4 = document.querySelector("#a1-4");
+
+var answer2_1 = document.querySelector("#a2-1");
+var answer2_2 = document.querySelector("#a2-2");
+var answer2_3 = document.querySelector("#a2-3");
+var answer2_4 = document.querySelector("#a2-4");
+
+var answer3_1 = document.querySelector("#a3-1");
+var answer3_2 = document.querySelector("#a3-2");
+var answer3_3 = document.querySelector("#a3-3");
+var answer3_4 = document.querySelector("#a3-4");
+
+var answer4_1 = document.querySelector("#a4-1");
+var answer4_2 = document.querySelector("#a4-2");
+var answer4_3 = document.querySelector("#a4-3");
+var answer4_4 = document.querySelector("#a4-4");
+
+var answer5_1 = document.querySelector("#a5-1");
+var answer5_2 = document.querySelector("#a5-2");
+var answer5_3 = document.querySelector("#a5-3");
+var answer5_4 = document.querySelector("#a5-4");
+
 
 // INITIALIZE
 var totalSeconds = 75;
 var status = true;
+var points = 0;
 question1.style.display = "none";
 question2.style.display = "none";
 question3.style.display = "none";
@@ -46,6 +74,68 @@ function renderTime() {
 }
 
 
+function plusPoint () {
+    points++;
+    console.log(points);
+}
+
+
+function toQ2 () {
+    question1.style.display = "none";
+    question2.style.display = "block";
+}
+function toQ3 () {
+    question2.style.display = "none";
+    question3.style.display = "block";
+}
+function toQ4 () {
+    question3.style.display = "none";
+    question4.style.display = "block";
+}
+function toQ5 () {
+    question4.style.display = "none";
+    question5.style.display = "block";
+}
+function toFin () {
+    question5.style.display = "none";
+    finish.style.display = "block";
+    showPoints.textContent = points;
+}
 
 // PRESSING START
 start.addEventListener("click", pressStart);
+
+// ANSWERING Q1
+answer1_1.addEventListener("click", toQ2);
+answer1_2.addEventListener("click", toQ2);
+answer1_3.addEventListener("click", plusPoint);
+    answer1_3.addEventListener("click", toQ2);
+answer1_4.addEventListener("click", toQ2);
+
+// ANSWERING Q2
+answer2_1.addEventListener("click", toQ3);
+answer2_2.addEventListener("click", toQ3);
+answer2_3.addEventListener("click", plusPoint);
+    answer2_3.addEventListener("click", toQ3);
+answer2_4.addEventListener("click", toQ3);
+
+// ANSWERING Q3
+answer3_1.addEventListener("click", toQ4);
+answer3_2.addEventListener("click", toQ4);
+answer3_3.addEventListener("click", toQ4);
+answer3_4.addEventListener("click", plusPoint);
+    answer3_4.addEventListener("click", toQ4);
+
+// ANSWERING Q4
+answer4_1.addEventListener("click", toQ5);
+answer4_2.addEventListener("click", toQ5);
+answer4_3.addEventListener("click", plusPoint);
+    answer4_3.addEventListener("click", toQ5);
+answer4_4.addEventListener("click", toQ5);
+
+// ANSWERING Q5
+answer5_1.addEventListener("click", toFin);
+answer5_2.addEventListener("click", toFin);
+answer5_3.addEventListener("click", toFin);
+answer5_4.addEventListener("click", plusPoint);
+    answer5_4.addEventListener("click", toFin);
